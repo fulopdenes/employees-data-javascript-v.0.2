baa6658080c28fd6b0b96c02af7a16eea067ab5e
Dénes <denes.fulopp@gmail.com>
2022-10-26 16:55:37 +0200

    insert Select, TextInput by MUI

--- client/src/Components/EmployeeTable/EmployeeTable.jsx
+++ client/src/Components/EmployeeTable/EmployeeTable.jsx
@@ -9,91 +9,189 @@ import TableRow from "@mui/material/TableRow";
 import DeleteIcon from "@mui/icons-material/Delete";
 import Paper from "@mui/material/Paper";
 
+import {
+  TextField,
+  Select,
+  MenuItem,
+  InputLabel,
+  OutlinedInput,
+  FormControl,
+  Checkbox,
+  ListItemText,
+} from "@mui/material";
+
+import React, { useState } from "react";
+import { useTheme } from "@mui/material/styles";
+
 const EmployeeTable = ({
   sortStatus,
   setSortStatus,
-  employees,
+  employees, //! ezzel dolgozunk
   onDelete,
   setSelectedColumnStatus,
   sortByColumn,
-}) => (
-  <TableContainer component={Paper}>
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell
-            onClick={(e) =>
-              sortByColumn(
-                e.target.id,
-                setSelectedColumnStatus,
-                sortStatus,
-                setSortStatus
-              )
-            }
-            id="name"
-            align="left"
-          >
-            Name
-          </TableCell>
-          {/* <TableCell align="left">Middle Name</TableCell>
-          <TableCell align="left">Last Name</TableCell> */}
-          <TableCell
-            id="level"
-            onClick={(e) =>
-              sortByColumn(
-                e.target.id,
-                setSelectedColumnStatus,
-                sortStatus,
-                setSortStatus
-              )
-            }
-            align="left"
-          >
-            Level
-          </TableCell>
-          <TableCell
-            onClick={(e) =>
-              sortByColumn(
-                e.target.id,
-                setSelectedColumnStatus,
-                sortStatus,
-                setSortStatus
-              )
-            }
-            id="position"
-            align="left"
-          >
-            Position
-          </TableCell>
-          <TableCell align="left"></TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {employees && employees.length
-          ? employees.map((employee) => (
-              <TableRow key={employee._id}>
-                <TableCell align="left">{employee.name}</TableCell>
-                <TableCell align="left">{employee.level}</TableCell>
-                <TableCell align="left">{employee.position}</TableCell>
-                <TableCell align="left">
-                  <Link to={`/update/${employee._id}`}>
-                    <Button variant="outlined">Update</Button>
-                  </Link>
-                  <Button
-                    onClick={() => onDelete(employee._id)}
-                    startIcon={<DeleteIcon />}
-                    variant="outlined"
-                    color="warning"
+  personName,
+  setPersonName,
+}) => {
+  const optionListValues = (column) => {
+    column.shift();
+    column.pop();
+    column.pop();
+    return column;
+  };
+
+  const [age, setAge] = useState("");
+  const [column, setColumn] = useState(
+    optionListValues(Object.keys(employees[0]))
+  ); //! we assume that all "documents" have the same "keys", so we take the structure of the document with index 0 as the starting point!
+
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250,
+      },
+    },
+  };
+
+  const names = [
+    "Oliver Hansen",
+    "Van Henry",
+    "April Tucker",
+    "Ralph Hubbard",
+    "Omar Alexander",
+    "Carlos Abbott",
+    "Miriam Wagner",
+    "Bradley Wilkerson",
+    "Virginia Andrews",
+    "Kelly Snyder",
+  ];
+
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === "string" ? value.split(",") : value
+    );
+  };
+
+  return (
+    <>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <FormControl sx={{ m: 1, width: 300 }}>
+                  <InputLabel id="demo-multiple-checkbox-label">Tag</InputLabel>
+                  <Select
+                    labelId="demo-multiple-checkbox-label"
+                    id="demo-multiple-checkbox"
+                    multiple
+                    value={personName}
+                    onChange={handleChange}
+                    input={<OutlinedInput label="Tag" />}
+                    renderValue={(selected) => selected.join(", ")}
+                    MenuProps={MenuProps}
                   >
-                    Delete
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))
-          : null}
-      </TableBody>
-    </Table>
-  </TableContainer>
-);
+                    {column.map((item) => (
+                      <MenuItem key={item} value={item}>
+                        <Checkbox checked={personName.indexOf(item) > -1} />
+                        <ListItemText primary={item} />
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </TableCell>
+              <TableCell>
+                <TextField
+                  id="outlined-basic"
+                  label="Outlined"
+                  variant="outlined"
+                  // margin="dense"
+                />
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell
+                onClick={(e) =>
+                  sortByColumn(
+                    e.target.id,
+                    setSelectedColumnStatus,
+                    sortStatus,
+                    setSortStatus
+                  )
+                }
+                id="name"
+                align="left"
+              >
+                Name
+              </TableCell>
+              {/* <TableCell align="left">Middle Name</TableCell>
+          <TableCell align="left">Last Name</TableCell> */}
+              <TableCell
+                id="level"
+                onClick={(e) =>
+                  sortByColumn(
+                    e.target.id,
+                    setSelectedColumnStatus,
+                    sortStatus,
+                    setSortStatus
+                  )
+                }
+                align="left"
+              >
+                Level
+              </TableCell>
+              <TableCell
+                onClick={(e) =>
+                  sortByColumn(
+                    e.target.id,
+                    setSelectedColumnStatus,
+                    sortStatus,
+                    setSortStatus
+                  )
+                }
+                id="position"
+                align="left"
+              >
+                Position
+              </TableCell>
+              <TableCell align="left"></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {employees && employees.length
+              ? employees.map((employee) => (
+                  <TableRow key={employee._id}>
+                    <TableCell align="left">{employee.name}</TableCell>
+                    <TableCell align="left">{employee.level}</TableCell>
+                    <TableCell align="left">{employee.position}</TableCell>
+                    <TableCell align="left">
+                      <Link to={`/update/${employee._id}`}>
+                        <Button variant="outlined">Update</Button>
+                      </Link>
+                      <Button
+                        onClick={() => onDelete(employee._id)}
+                        startIcon={<DeleteIcon />}
+                        variant="outlined"
+                        color="warning"
+                      >
+                        Delete
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))
+              : null}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+};
 
 export default EmployeeTable;
--- client/src/Pages/EmployeeList.jsx
+++ client/src/Pages/EmployeeList.jsx
@@ -20,6 +20,8 @@ const EmployeeList = () => {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState(null);
 
+  const [personName, setPersonName] = useState([]);
+
   const handleDelete = (id) => {
     deleteEmployee(id).catch((err) => {
       console.log(err);
@@ -29,10 +31,90 @@ const EmployeeList = () => {
       return employees.filter((employee) => employee._id !== id);
     });
   };
-  const sortByColumn = (columnName, stateSetter, status, setStatus) => {
+  console.log(personName);
+  const sortByColumn = (
+    columnName,
+    stateSetter,
+    status,
+    setStatus
+    //! nem szabad ide meghivni a "personName useState-t"
+  ) => {
     setStatus(!status);
     const ascOrDesc = status ? "asc" : "desc";
     stateSetter(columnName);
+
+    console.log(personName);
+
+    // const map1 = personName.map((x) => {
+    //   if (x === "startingDate") {
+    //     return "Starting Date";
+    //   } else if (x === "firstName") {
+    //     return "First Name";
+    //   } else if (x === "middleName") {
+    //     return "Middle Name";
+    //   } else if (x === "lastName") {
+    //     return "Last Name";
+    //   } else if (x === "currentSalary") {
+    //     return "Current Salary";
+    //   } else if (x === "desiredSalary") {
+    //     return "Desired Salary";
+    //   } else if (x === "level") {
+    //     return "Level";
+    //   } else if (x === "position") {
+    //     return "Position";
+    //   } else if (x === "favColor") {
+    //     return "Favorite Color";
+    //   } else if (x === "boss") {
+    //     return "Boss";
+    //   } else {
+    //     return "";
+    //   }
+    // });
+    // console.log(map1);
+
+    const map1 = personName.map((x) => {
+      if (x === "startingDate") {
+        return "toSort=startingDate";
+      } else if (x === "firstName") {
+        return "toSort=firstName";
+      } else if (x === "middleName") {
+        return "toSort=middleName";
+      } else if (x === "lastName") {
+        return "toSort=lastName";
+      } else if (x === "currentSalary") {
+        return "toSort=currentSalary";
+      } else if (x === "desiredSalary") {
+        return "toSort=desiredSalary";
+      } else if (x === "level") {
+        return "toSort=level";
+      } else if (x === "position") {
+        return "toSort=position";
+      } else if (x === "favColor") {
+        return "toSort=favColor";
+      } else if (x === "boss") {
+        return "toSort=boss";
+      } else {
+        return "";
+      }
+    });
+    console.log(map1);
+    console.log(map1.join("&"));
+
+    // const keyPairList = getValue(personName);
+    // function getValue(personName) {
+    //   const tableCellList = ["Name", "Level", "Position"];
+    //   //ha a (personName[i].lowercase()).includes()
+
+    //   const map1 = personName.filter((x) => x.includes("name"));
+    //   console.log(map1);
+    // }
+    // const hello = (personName) => console.log(personName);
+    // personName.map((items) => {
+    //   console.log(items);
+    //   return items;
+    // });
+
+    // console.log(hello());
 
     const malacka = "sort=" + columnName + "&" + "by=" + ascOrDesc;
     console.log(malacka);
@@ -70,7 +152,6 @@ const EmployeeList = () => {
   if (loading) {
     return <Loading />;
   }
-
   return (
     <EmployeeTable
       sortStatus={sortStatus}
@@ -79,6 +160,8 @@ const EmployeeList = () => {
       setSelectedColumnStatus={setSelectedColumnStatus}
       employees={data}
       onDelete={handleDelete}
+      personName={personName}
+      setPersonName={setPersonName}
     />
   );
 };
